a80c53d850e8739aa51c59d66479be9040ca58f9
ahasan <ahasan@atilimited.net>
2020-10-19 10:17:48 +0600

    test

--- c++/IncreasingArray.cpp
+++ c++/IncreasingArray.cpp
@@ -5,42 +5,4 @@
  *      Author: ahasan
  */
 
-#include <iostream>
-
-using namespace std;
-
-typedef long long ll;
-
-int main() {
-  int n;
-  cin >> n;
-  ll sum = 0;
-  int a, b;
-  cin >> a;
-  for (int i = 1; i < n; i++) {
-    cin >> b;
-    if (b < a) sum += a - b;
-    else a = b;
-  }
-  cout << sum << endl;
-}
-
-#include <iostream>
-
-using namespace std;
-
-typedef long long ll;
-
-int main() {
-  int n;
-  cin >> n;
-  ll sum = 0;
-  int a, b;
-  cin >> a;
-  for (int i = 1; i < n; i++) {
-    cin >> b;
-    if (b < a) sum += a - b;
-    else a = b;
-  }
-  cout << sum << endl;
-}
+dfjsdbgjsdgbjsdgbjsgbjsfdbgjbcvbxjcgvbcjxvb
