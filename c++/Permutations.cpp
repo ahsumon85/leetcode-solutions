f5704799592aa5c2057609141167bf2b86860f02
Ahasan Habib <habibsumoncse@gmail.com>
2020-10-21 10:22:56 +0600

    Permutations using c++

---
+++ c++/Permutations.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  if (n == 1) cout << "1\n";
+  else if (n < 4) cout << "NO SOLUTION\n";
+  else if (n == 4) cout << "3 1 4 2\n";
+  else {
+    for (int i = 1; i <= n; i += 2) cout << i << " ";
+    for (int i = 2; i <= n; i += 2) {
+      cout << i;
+      if (i < n - 1) cout << " ";
+      else cout << endl;
+    }
+  }
+}
